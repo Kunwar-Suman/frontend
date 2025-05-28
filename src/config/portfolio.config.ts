606842bb87482473553a5a7d76dda0238a70572c
Suman Kunwar <sumankunwar8848@gmail.com>
2025-05-28 11:32:20 +0545

    rebase to the main branch

---
+++ src/config/portfolio.config.ts
@@ -0,0 +1,327 @@
+export const portfolioConfig = {
+  // Basic Information
+  name: "Suman Kunwar",
+  title: "Software Engineer",
+  description: "I am a Passionate Software Developer",
+  location: "Kathmandu, Nepal",
+
+  // Contact Information
+  email: "sumankunwar8848@gmail.com",
+
+  // Social Media Links
+  socialLinks: {
+    facebook: "https://www.facebook.com/suman-kunwar",
+    twitter: "https://twitter.com/suman-kunwar",
+    linkedin: "https://www.linkedin.com/in/suman-kunwar",
+    external: "https://tinyurl.com/suman-kunwar",
+    github: "https://github.com/Kunwar-Suman", 
+  },
+
+  // SEO Information
+  seo: {
+    ogImage: "https://sumankunwar.vercel.app/og-image.png",
+    url: "https://sumankunwar.vercel.app",
+    twitterHandle: "@Sumankunwar7",
+    keywords: [
+      "SK",
+      "Suman",
+      "Kunwar",
+      "SK",
+      "SK",
+      "Suman",
+      "Suman",
+      "Kunwar",
+      "Kunwar",
+      "SK Suman Kunwar",
+      "SK Suman Kunwar",
+      "SKSuman",
+      "SKSumanKunwar",
+      "Suman Kunwar",
+      "Suman Kunwar",
+      "SumanKunwar",
+      "portfolio",
+      "web developer",
+      "web",
+      "web dev",
+      "developer",
+      "PROGRAMMER ",
+      "programmer ",
+      "SK Suman Kunwar ",
+      "website",
+      "@SumanKunwar",
+      "SumanKunwar",
+      "Suman developer",
+    ],
+    authors: [
+      {
+        name: "Suman Kunwar",
+        url: "https://github.com/Kunwar-Suman",
+      },
+    ],
+  },
+
+  // About Information
+  about: {
+    bio: "I am a Full Stack Web Developer from Kathmandu, Nepal. I'm passionate about crafting web projects and contributing to open-source communities. I specialize in modern JavaScript frameworks and responsive CSS design, focusing on creating pixel-perfect, user-friendly interfaces. With strong attention to detail and efficient delivery, I build seamless web experiences that combine functionality with elegant design.",
+    hobbies: [
+      "Coding",
+      "Playing Games",
+      "Watching Series",
+      "Tech Blog Writing",
+      "Travelling",
+    ],
+    personalInfo: {
+      language: "Nepali, English",
+      nationality: "Nepal",
+      gender: "Male",
+    },
+  },
+
+  // Skills and Roles
+  skills: {
+    roles: [
+      "Freelancer",
+      "Blogger",
+      "Gamer",
+      "Creator",
+      "Student",
+      "Contributer",
+    ],
+    // Technical Skills
+    programmingLanguages: [
+      {
+        name: "HTML5",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      },
+      {
+        name: "CSS3",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+      },
+      {
+        name: "JavaScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "TypeScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "C++",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+      },
+    ],
+    frameworks: [
+      {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      },
+      {
+        name: "Next.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      },
+      {
+        name: "Vue.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+      },
+      {
+        name: "Svelte",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        name: "Bootstrap",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+      },
+      {
+        name: "Material UI",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+      },
+    ],
+    tools: [
+      {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+      },
+      {
+        name: "VS Code",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      },
+      {
+        name: "Docker",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+      },
+      {
+        name: "Firebase",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+      },
+      {
+        name: "MongoDB",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      },
+      {
+        name: "PostgreSQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+      },
+    ],
+  },
+
+  // Education Information
+  education: [
+    {
+      degree: "Bachelor of Computer Science and Information Technology",
+      institution: "Tribhuvan University",
+      location: "Kathmandu, Nepal",
+      period: "July 2019 - 2024",
+      description:
+        "I am recently graduated student from Tribhuvan University. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
+    },
+  ],
+
+  // Projects Information
+  projects: [
+    {
+      title: "WePost- A modern, minimalistic social platform.",
+      description:
+        "WePost is a modern, minimalistic social platform for sharing posts, like, follow, and engaging in comments etc.",
+      tags: ["Tanstack-Query", "PostgreSQL", "Shadcn Ui", "Nextjs"],
+      link: "https://we-post-dev.vercel.app",
+    },
+    {
+      title: "Devzone- A community chat app for devs",
+      description:
+        "DevZone is the ultimate chat platform for developers to connect, collaborate, and code together. Join the conversation and level up your coding journey.",
+      tags: ["Zustand", "Supabase", "Shadcn Ui", "Nextjs"],
+      link: "https://devzone-ti.vercel.app/",
+    },
+    {
+      title: "itZmyLink- One Page many Links",
+      description:
+        "itZmyLink is a simple platform where user can create a personalized page to showcase all your social media profiles in one place. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/Suman-786/itZmyLink",
+    },
+    {
+      title: "GitEstimate- Github estimate worth generator",
+      description:
+        "GitEstimate is a simple fun tool where user can generate their github estimate worth card just by entering their github username. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/Suman-786/GitEstimate",
+    },
+    {
+      title: "Mixcn-ui- Reusable components for Nextjs",
+      description:
+        "This is Nextjs app with a Collection of Nextjs Components - (Currently under Devlelopment) ",
+      tags: ["Nextjs", "Shadcnui", "Npx", "Library"],
+      link: "https://mixcn-ui.vercel.app",
+    },
+    {
+      title: "Rupeespot- All deals Product in one place",
+      description:
+        "This is a free tool to check price history charts for millions of products for popular Indian stores such as Amazon and Flipkart. ",
+      tags: ["Nextjs", "Freelancing", "MySQL"],
+      link: "https://rupeespot.com/",
+    },
+    {
+      title: "Formcraft- Build Type-Safe Forms with Zero Effort",
+      description:
+        "Create production-ready forms with Zod validation, React Hook Form, and TypeScript. Design, customize, and deploy in minutes.",
+      tags: ["Nextjs", "Shadcnui", "Zod", "React Hook Form "],
+      link: "https://formcraft-ti.vercel.app/",
+    },
+    {
+      title: "KryptoKapital- Investing in Cryptocurreny",
+      description:
+        "This is a platform for learning about cryptocurrency with many tools and festures. It is very big project. ",
+      tags: ["Nextjs", "Freelancing", "Supabase"],
+      link: "https://pro.kryptokapital.info/",
+    },
+    {
+      title: "FriendZ - A social media Platform",
+      description:
+        "FriendZ is a social media app made with modern tech stacks such as redis, Authjs etc. A user can Create, delete, like, comment Post. ",
+      tags: ["Redis", "Authjs", "Typescript", "Nextjs"],
+      link: "https://github.com/Suman-786/project-friendz",
+    },
+    {
+      title: "ChatApp- Simple instagram clone",
+      description:
+        "ChatApp is similar to instagram, you can create, edit, delete, like,comment, post and  Follow, unfollow users",
+      tags: ["Reactjs", "Javascript"],
+      link: "https://github.com/Suman-786/chatapp-api",
+    },
+    {
+      title: "Devletter - A Newsletter for devs",
+      description:
+        "Devletter ia a newsletter subscribing single page app made with Nextjs and Tailwindcss.",
+      tags: ["Nextjs", "Tailwindcss"],
+      link: "https://github.com/Suman-786/Devletter",
+    },
+    {
+      title: "CrouMaker - A Crousal Maker App",
+      description:
+        "Crousal Maker is a tool with in-built crousals templates edit and download it in any format.",
+      tags: ["Nextjs", "jsPDF", "html2canvas", "Shadcn Ui"],
+      link: "https://github.com/Suman-786/crousal-maker",
+    },
+  ],
+
+  // More Links Information
+  moreLinks: [
+    {
+      title: "Dev.to",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Dev.to ",
+      link: "https://dev.to/random_ti",
+    },
+    {
+      title: "Hashnode",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Hashnode",
+      link: "https://SKSumanKunwar.hashnode.dev/",
+    },
+    {
+      title: "Medium",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Medium",
+      link: "https://medium.com/@SKSuman.jhar",
+    },
+    {
+      title: "Daily.Dev",
+      description:
+        "I am also the member of Daily Dev squads and i also upload post on some squads.",
+      link: "https://app.daily.dev/Suman_786",
+    },
+    {
+      title: "Gumroad",
+      description:
+        "I also sell digital products on gumroad like Notion Templates and web projects etc.",
+      link: "https://SKSumanjhar.gumroad.com/",
+    },
+    {
+      title: "Buy Me a coffee",
+      description:
+        "Supports my content quality, new topics, and project creation.",
+      link: "https://www.buymeacoffee.com/Sumandevloper",
+    },
+  ],
+
+  // API Keys (should be in .env but referenced here)
+  apiKeys: {
+    resendApiKey: "YOUR_RESEND_API_KEY", // This should be loaded from environment variables in a real app
+  },
+};
